bdfb9a0a4b37beb8b5c48dc7f5f33cb3d525fccf
deepu426 <70310300+deepu426@users.noreply.github.com>
2020-12-21 21:13:03 +0530

    Add files via upload

---
+++ insta.py
@@ -0,0 +1,19 @@
+import instaloader 
+#banner of script
+print ( """
+██╗███╗   ██╗███████╗████████╗ █████╗ 
+██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗
+██║██╔██╗ ██║███████╗   ██║   ███████║
+██║██║╚██╗██║╚════██║   ██║   ██╔══██║
+██║██║ ╚████║███████║   ██║   ██║  ██║
+╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝
+""" ) 
+
+print ('       <===[[coded by deepz ]]===>  \n')
+print ('       <==={{hack the planate}}===>  \n')
+
+
+
+mod=instaloader.Instaloader()
+a=input('enter username ===>> ')
+mod.download_profile(a,profile_pic_only=True)
